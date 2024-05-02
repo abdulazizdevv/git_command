ceb6adf463feeb353b9a5d33d8cdac883895bfdb
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2024-05-02 12:28:39 +0500

    add staging

--- src/components/StagingTest.tsx
+++ src/components/StagingTest.tsx
@@ -1,18 +1,5 @@
 const StagingTest = () => {
-  return (
-    <div>
-      <p>StagingTest</p>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga,
-        necessitatibus eaque sunt quas excepturi id, recusandae consectetur
-        doloremque error quis magni, commodi velit ea voluptates enim. Nisi
-        obcaecati officiis necessitatibus, aut quo illo. Necessitatibus
-        asperiores magni consectetur aliquam dolor nobis. Temporibus, quis sint
-        esse laborum eius eligendi perspiciatis quibusdam.
-      </p>
-      <h1>THE BEST STAGING TEXT</h1>
-    </div>
-  );
+  return <div>StagingTest</div>;
 };
 
 export default StagingTest;
