f651d29b03a1cce2235f817a7c97d5ef2ad6da4e
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2024-05-02 12:29:48 +0500

    add lorem

--- src/components/StagingTest.tsx
+++ src/components/StagingTest.tsx
@@ -1,5 +1,17 @@
 const StagingTest = () => {
-  return <div>StagingTest</div>;
+  return (
+    <div>
+      <p>StagingTest</p>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi fuga,
+        necessitatibus eaque sunt quas excepturi id, recusandae consectetur
+        doloremque error quis magni, commodi velit ea voluptates enim. Nisi
+        obcaecati officiis necessitatibus, aut quo illo. Necessitatibus
+        asperiores magni consectetur aliquam dolor nobis. Temporibus, quis sint
+        esse laborum eius eligendi perspiciatis quibusdam.
+      </p>
+    </div>
+  );
 };
 
 export default StagingTest;
