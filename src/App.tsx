978c7ded5e59da6dc1d01f7cc19a5e2069abf8a4
abdulazizdevv <abdulazizkenjaoxunov@gmail.com>
2024-05-02 12:27:12 +0500

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import RevertTest from './components/RevertTest';
+
+function App() {
+  return (
+    <>
+      <p>Test</p>
+      <RevertTest />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/RevertTest.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const RevertTest = () => {
+  return <div>RevertTest</div>;
+};
+
+export default RevertTest;
